825b558d02fe6bccaa86f9eb6783b3bb39ef07ac
britko <britko@naver.com>
2022-11-14 15:14:41 +0900

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+## kubectl 설정
 # 패키지 설치
 sudo yum update
 sudo yum install bash-completion -y
@@ -10,3 +11,9 @@ echo 'source <(kubectl completion bash)' >>~/.bashrc
 # kubectl(k) alias 설정
 echo 'alias k=kubectl' >>~/.bashrc
 echo 'complete -F __start_kubectl k' >>~/.bashrc
+
+
+## Helm 설치
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh

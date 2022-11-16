780d9e096c7a409f667cc20130e50f24364edcfc
EC2 Default User <ec2-user@ip-172-31-32-189.ap-northeast-2.compute.internal>
2022-11-16 23:29:13 +0000

    Monitoring configuration complete

---
+++ monitoring/monitoring.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# https://may9noy.tistory.com/303
+# Add helm repository
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts 
+helm repo add grafana https://grafana.github.io/helm-charts 
+helm repo update
+
+# Deploy prometheus & grafana by helm
+kubectl create ns monitoring
+helm install prometheus prometheus-community/prometheus -f values-prometheus.yaml -n monitoring
+helm install grafana grafana/grafana -f values-grafana.yaml -n monitoring
+
+
+# 배포 확인
+kubectl get pod,svc -n prometheus
